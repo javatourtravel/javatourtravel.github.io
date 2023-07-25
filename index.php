9d533793362e2c6ed2e84e1bb072a2c227bc8420
javatourtravel <140345043+javatourtravel@users.noreply.github.com>
2023-07-26 02:24:18 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>JAVA Tour & Travel</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/logo.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  
+</head>
+
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <a href="#hero"><img src="img/logo.png" alt="" title="" /></a>
+        <!-- Uncomment below if you prefer to use a text logo -->
+        <!--<h1><a href="#hero">Regna</a></h1>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#hero">Home</a></li>
+          <li><a href="#about">Tentang Kami</a></li>
+          <li><a href="#services">Paket Tour</a></li>
+          <li><a href="#portfolio">Dokumentasi</a></li>
+          <!---<li><a href="#team">Team</a></li>--->
+          <li><a href="#contact">Kontak Kami</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Hero Section
+  ============================-->
+  <section id="hero">
+    <div class="hero-container">
+      
+      <h1>JAVA Tour & Travel</h1>
+      <h2>Keceriaan anda dalam berwisata adalah tujuan kami</h2>
+      <a href="#about" class="btn-get-started">Lebih Lanjut</a>
+    </div>
+  </section><!-- #hero -->
+
+  <main id="main">
+
+    <!--==========================
+      About Us Section
+    ============================-->
+    <section id="about">
+      <div class="container">
+        <div class="row about-container">
+
+          <div class="col-lg-6 content order-lg-1 order-2">
+            <h2 class="title">JAVA Tour & Travel</h2>
+            <p>
+              
+            </p>
+
+            <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-photo"></i></div>
+              <h4 class="title"><a href="">Perjalanan Wisata</a></h4>
+              <p class="description">Kami menyediakan berbagai paket perjalanan wisata, Study Excursie maupun wisata religi. Beberapa diantaranya 
+              adalah perjalanan wisata ke Jakarta, Bandung, Bali dan Lombok, Yogjakarta, dll</p>
+            </div>
+
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+              <div class="icon"><i class="fa fa-photo"></i></div>
+              <h4 class="title"><a href="">Travel</a></h4>
+              <p class="description">Kami juga menyediakan jasa travel dengan fasilitas yang memuaskan. kami memberikan pelayanan terbaik
+              bagi pelanggan sehingga perjalanan akan semakin menyenangkan</p>
+            </div>
+
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+              <div class="icon"><i class="fa fa-photo"></i></div>
+              <h4 class="title"><a href="">Rental mobil</a></h4>
+              <p class="description">Disini kami juga menyewakan mobil dengan berbagai tipe dan merek. kami juga menyediakan sopir apabila anda tidak mau berlelah-lelah mengemudikan kendaraan sendiri.</p>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6 order-lg-2 order-1 wow fadeInRight"><iframe style="margin-top: 20%;margin-bottom: 10%;min-height: 350px;width: 100%;"  width="" height="" src="https://www.youtube.com/embed/ctJbcCcecwQ?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+        </div>
+
+      </div>
+    </section><!-- #about -->
+
+    <!--==========================
+      Facts Section
+    ============================-->
+    <section id="facts">
+      <div class="container wow fadeIn">
+        <div class="section-header">
+          
+        </div>
+
+      </div>
+    </section><!-- #facts -->
+
+    <!--==========================
+      Services Section
+    ============================-->
+    <section id="services">
+      <div class="container wow fadeIn">
+        <div class="section-header">
+          <h3 class="section-title">Paket Tour</h3>
+          <p class="section-description">berikut adalah beberapa paket tour yang kami sediakan</p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="box1">
+              <div class="icon"><a href=""><i class="fa fa-paper-plane"></i></a></div>
+              <h4 class="title"><a href="">Bali Inap 2 malam</a></h4>
+              <p class="description">Paket Tour Ke Bali menginap 2 malam dihotel berbintang. Tujuan objek wisata diantaranya Pantai Tanah Lot, Tanjung Benoa,
+              Pantai Pandawa, GWK, ...</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+            <div class="box2">
+              <div class="icon"><a href=""><i class="fa fa-paper-plane"></i></a></div>
+              <h4 class="title"><a href="">Jogjakarta</a></h4>
+              <p class="description">Paket tour ke DIY Jogjakarta 1 hari dengan tujuan Objek wisata Candi Borobudur, Pemandian Mendhut, Taman Pintar, Museum Dirgantara, Malio...</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+            <div class="box3">
+              <div class="icon"><a href=""><i class="fa fa-paper-plane"></i></a></div>
+              <h4 class="title"><a href="">Malang</a></h4>
+              <p class="description">Paket tour ke Batu Malang dengan tujuan objek wisata Jatim Park Grup, Selecta, Petik Apel, Hawai Waterpark... </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #services -->
+
+    <!--==========================
+    Call To Action Section
+    ============================-->
+    <section id="call-to-action">
+      <div class="container wow fadeIn">
+        <div class="row">
+          <div class="col-lg-9 text-center text-lg-left">
+            <h3 class="cta-title">Bantuan Seputar Paket Tour</h3>
+            <p class="cta-text">Jangan kawatir apabila tujuan wisata anda tidak tercantum pada paket tour kami, Kami akan membantu anda untuk mempermudah perjalanan wisata anda.<br><br> Silahkan menghubungi offcial Team kami untuk konsultasi lebih lanjut.</p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="#team">Kontak Team</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #call-to-action -->
+
+    <!--==========================
+      Portfolio Section
+    ============================-->
+    <section id="portfolio">
+      <div class="container wow fadeInUp">
+        <div class="section-header">
+          <h3 class="section-title">Dokumentasi</h3>
+          <p class="section-description">Berikut adalah beberapa foto dokumentasi perjalanan wisata kami.</p>
+        </div>
+        <div class="row">
+
+          <div class="col-lg-12">
+            <ul id="portfolio-flters">
+              <li data-filter=".filter-bali, .filter-jg, .filter-ml, .filter-ob" class="filter-active">All</li>
+              <li data-filter=".filter-bali">Bali</li>
+              <li data-filter=".filter-jg">Jogja</li>
+              <li data-filter=".filter-ml">Malang</li>
+              <li data-filter=".filter-ob">Outbond</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row" id="portfolio-wrapper">
+          <div class="col-lg-4 col-md-8 portfolio-item filter-ob">
+            <a href="">
+              <img src="img/portfolio/app1.jpg" alt="" height="200" width="380">
+              <div class="details">
+                <h4>Outbond</h4>
+                <span>Kegiatan outbond rombongan UPTD Puskesmas Baron</span>
+              </div>
+            </a>
+          </div>
+
+          
+
+          <div class="col-lg-4 col-md-8 portfolio-item filter-ob">
+            <a href="">
+              <img src="img/portfolio/app3.jpg" alt="" height="200" width="380">
+              <div class="details">
+                <h4>Outbond</h4>
+                <span>Kegiatan outbond rombongan UPTD Puskesmas Baron</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-8 portfolio-item filter-bali">
+            <a href="">
+              <img src="img/portfolio/card1.jpg" alt="" height="200" width="380">
+              <div class="details">
+                <h4>Tour Bali</h4>
+                <span>Rombongan study tour MTSN 3 Nganjuk</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-8 portfolio-item filter-bali">
+            <a href="">
+              <img src="img/portfolio/card2.jpg" alt="" height="200" width="380">
+              <div class="details">
+                <h4>Tour Bali</h4>
+                <span>Rombongan study tour MTSN 3 Nganjuk</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-8 portfolio-item filter-bali">
+            <a href="">
+              <img src="img/portfolio/card3.jpg" alt="" height="200" width="380">
+              <div class="details">
+                <h4>Tour Bali</h4>
+                <span>Rombongan study tour MTSN 3 Nganjuk</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-8 portfolio-item filter-ml">
+            <a href="">
+              <img src="img/portfolio/space1.jpg" alt="" height="200" width="380">
+              <div class="details">
+                <h4>Malang</h4>
+                <span>Rombongan wisata kebun apel Batu</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-8 portfolio-item filter-ml">
+            <a href="">
+              <img src="img/portfolio/space2.jpg" alt="" height="200" width="380">
+              <div class="details">
+                <h4>Malang</h4>
+                <span>Rombongan wisata kebun apel Batu</span>
+              </div>
+            </a>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #portfolio -->
+
+    
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>JAVA Tour & Travel</h3>
+            <p>Menyediakan paket tour dan travel terbaik untuk anda.</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Kontak Kami</h4>
+            <p>
+              Jl. Simpang 4 RT. 02/RW. 05<br>
+              Kabupaten Nganjuk<br>
+              Jawa Timur<br>
+              <strong>Phone:</strong> +62 852-3171-8032<br>
+              <strong>Email:</strong> info@javatnt.com<br>
+            </p>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Navigasi</h4>
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#hero">Home</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#about">Tentang Kami</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#services">Paket Tour</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#portfolio">Dokumentasi</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#contact">Kontak Kami</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Pelayanan Kami</h4>
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Pemesanan Tiket</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Pemesanan Hotel</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Pemesanan Paket Tour</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="social-links">
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
